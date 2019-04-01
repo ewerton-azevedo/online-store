306de61160b3b664c5460e2076c2d117549ab573
Ewerton Azevedo <ewerton.azevedo@live.co.uk>
2019-04-01 13:29:30 +0200

    Module2 Finished

--- starter-files/app.js
+++ starter-files/app.js
@@ -13,14 +13,12 @@ const routes = require('./routes/index');
 const helpers = require('./helpers');
 const errorHandlers = require('./handlers/errorHandlers');
 
-// create our Express app
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views')); // this is the folder where we keep our pug files
-app.set('view engine', 'pug'); // we use the engine pug, mustache or EJS work great too
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'pug');
 
-// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Takes the raw requests and turns them into usable properties on req.body
@@ -33,8 +31,6 @@ app.use(expressValidator());
 // populates req.cookies with any cookies that came along with the request
 app.use(cookieParser());
 
-// Sessions allow us to store data on visitors from request to request
-// This keeps users logged in and allows us to send flash messages
 app.use(session({
   secret: process.env.SECRET,
   key: process.env.KEY,
