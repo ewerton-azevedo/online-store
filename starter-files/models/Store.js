a7773b78af238f505b9ca065d068304dcd976b75
Ewerton Azevedo <ewerton.azevedo@live.co.uk>
2019-04-01 16:52:47 +0200

    Saving Lat and Long for stores

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -17,7 +17,31 @@ const storeSchema = new mongoose.Schema({
         trim: true
     },
 
-    tags:[String]
+    tags:[String], 
+
+    created: {
+        type: Date,
+        default: Date.now
+    },
+
+    location:{
+        type: {
+            type: String,
+            default: 'Point'
+        },
+
+        coordinates: [
+            {
+                type: Number,
+                required: 'You must supply coordinates!'
+            }
+        ],
+
+        address: {
+            type: String,
+            required: 'You must supply an address!'
+        }
+    }
 
 })
 
